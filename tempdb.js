448ab7be7c1791171ffc0db47ee35e9d320a4af7
team12xd <botxd2033@gmail.com>
2024-07-02 09:36:38 +0200

    Add files via upload

---
+++ tempdb.js
@@ -0,0 +1,22 @@
+//nteej
+const mongoose = require("mongoose");
+const TempDb = new mongoose.Schema({
+  id: {
+    type: String,
+    unique: true,
+    required: true,
+    default: "NTEEJ-MD"
+  },
+  creator: {
+    type: String,
+    default: "Nteej"
+  },
+  data: {
+    type: Object,
+    default: {}
+  }
+});
+const dbtemp = mongoose.model("dbtemp", TempDb);
+module.exports = {
+  dbtemp
+};
