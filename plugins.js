99666450aa59b3131c79946727f201e264b87ab2
team12xd <botxd2033@gmail.com>
2024-07-02 09:41:23 +0200

    Add files via upload

--- plugins.js
+++ plugins.js
@@ -1,57 +1,16 @@
-//nteej
-const config = require("../config");
-const commands = [];
+const fs = require('fs');
+const path = require('path');
+const directoryPath = '../plugins';
+let jsFileCount = 0;
 
-function cmd(cmdObj, handler) {
-  const command = cmdObj;
-  command.function = handler;
-
-  if (!command.pattern && cmdObj.cmdname) {
-    command.pattern = cmdObj.cmdname;
-  }
-
-  if (!command.alias) {
-    command.alias = [];
-  }
-
-  if (!command.dontAddCommandList) {
-    command.dontAddCommandList = false;
-  }
-
-  if (!command.desc) {
-    command.desc = cmdObj.info ? cmdObj.info : "";
-  }
-
-  if (!command.fromMe) {
-    command.fromMe = false;
-  }
-
-  if (!command.category) {
-    command.category = cmdObj.type ? cmdObj.type : "misc";
-  }
-
-  command.info = command.desc;
-  command.type = command.category;
-
-  if (!command.use) {
-    command.use = "";
-  }
-
-  if (!command.filename) {
-    command.filename = "Not Provided";
-  }
-
-  commands.push(command);
-  return command;
-}
-
-const Module = { export: cmd };
-module.exports = {
-  cmd,
-  AddCommand: cmd,
-  Function: cmd,
-  Module,
-  smd: cmd,
-  commands,
-  bot: cmd,
-};
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    files.forEach(function (file) {
+        if (file.endsWith('.js')) {
+            jsFileCount++;
+        }
+    });
+    console.log(`Found ${jsFileCount} .js files in the directory.`);
+});
